ca41a1b65c723f7c044b60575d8820c9664a5a88
Flow86 <github@fd.mytrap.de>
2015-08-08 11:07:06 +0200

    fix copyright year

--- src/liblobby.h
+++ src/liblobby.h
@@ -1,6 +1,6 @@
 // $Id: liblobby.h 7292 2011-07-12 12:48:19Z FloSoft $
 //
-// Copyright (c) 2005 - 2011 Settlers Freaks (sf-team at siedler25.org)
+// Copyright (c) 2005 - 2015 Settlers Freaks (sf-team at siedler25.org)
 //
 // This file is part of Return To The Roots.
 //
