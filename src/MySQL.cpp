1376d7817134464f0952d1bfaeba4d8891d77bf2
Alexander Grund <Alex@grundis.de>
2016-01-29 00:54:09 +0100

    Add DebugNew.h

--- src/MySQL.cpp
+++ src/MySQL.cpp
@@ -21,13 +21,8 @@
 #include "MySQL.h"
 #include <stdio.h>
 
-///////////////////////////////////////////////////////////////////////////////
-// Makros / Defines
-#if defined _WIN32 && defined _DEBUG
-#define new new(_NORMAL_BLOCK, THIS_FILE, __LINE__)
-#undef THIS_FILE
-static char THIS_FILE[] = __FILE__;
-#endif
+// Include last!
+#include "DebugNew.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 // Konstruktor
