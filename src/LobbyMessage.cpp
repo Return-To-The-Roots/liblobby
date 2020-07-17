9275cbfa2303cc8235e96f275829be0d84efd3a4
Alexander Grund <Flamefire@users.noreply.github.com>
2020-07-17 13:06:15 +0200

    Reformat code

--- src/LobbyMessage.cpp
+++ src/LobbyMessage.cpp
@@ -16,65 +16,29 @@ Message* LobbyMessage::create_lobby(unsigned short id)
     {
         default: return create_base(id);
 
-        case NMS_LOBBY_LOGIN: { msg = new LobbyMessage_Login();
-        }
-        break;
-        case NMS_LOBBY_LOGIN_DONE: { msg = new LobbyMessage_Login_Done();
-        }
-        break;
-        case NMS_LOBBY_LOGIN_ERROR: { msg = new LobbyMessage_Login_Error();
-        }
-        break;
+        case NMS_LOBBY_LOGIN: msg = new LobbyMessage_Login(); break;
+        case NMS_LOBBY_LOGIN_DONE: msg = new LobbyMessage_Login_Done(); break;
+        case NMS_LOBBY_LOGIN_ERROR: msg = new LobbyMessage_Login_Error(); break;
 
-        case NMS_LOBBY_SERVERLIST: { msg = new LobbyMessage_ServerList();
-        }
-        break;
-        case NMS_LOBBY_SERVERINFO: { msg = new LobbyMessage_ServerInfo();
-        }
-        break;
-        case NMS_LOBBY_RANKINGLIST: { msg = new LobbyMessage_RankingList();
-        }
-        break;
-        case NMS_LOBBY_PLAYERLIST: { msg = new LobbyMessage_PlayerList();
-        }
-        break;
+        case NMS_LOBBY_SERVERLIST: msg = new LobbyMessage_ServerList(); break;
+        case NMS_LOBBY_SERVERINFO: msg = new LobbyMessage_ServerInfo(); break;
+        case NMS_LOBBY_RANKINGLIST: msg = new LobbyMessage_RankingList(); break;
+        case NMS_LOBBY_PLAYERLIST: msg = new LobbyMessage_PlayerList(); break;
 
-        case NMS_LOBBY_ID: { msg = new LobbyMessage_Id();
-        }
-        break;
-        case NMS_LOBBY_CHAT: { msg = new LobbyMessage_Chat();
-        }
-        break;
+        case NMS_LOBBY_ID: msg = new LobbyMessage_Id(); break;
+        case NMS_LOBBY_CHAT: msg = new LobbyMessage_Chat(); break;
 
-        case NMS_LOBBY_PING: { msg = new LobbyMessage_Ping();
-        }
-        break;
-        case NMS_LOBBY_PONG: { msg = new LobbyMessage_Pong();
-        }
-        break;
+        case NMS_LOBBY_PING: msg = new LobbyMessage_Ping(); break;
+        case NMS_LOBBY_PONG: msg = new LobbyMessage_Pong(); break;
 
-        case NMS_LOBBY_SERVER_ADD: { msg = new LobbyMessage_Server_Add();
-        }
-        break;
-        case NMS_LOBBY_SERVER_ADD_FAILED: { msg = new LobbyMessage_Server_Add_Failed();
-        }
-        break;
-        case NMS_LOBBY_SERVER_DELETE: { msg = new LobbyMessage_Server_Delete();
-        }
-        break;
-        case NMS_LOBBY_SERVER_UPDATE_PLAYER: { msg = new LobbyMessage_Server_Update_Player();
-        }
-        break;
-        case NMS_LOBBY_SERVER_UPDATE_MAP: { msg = new LobbyMessage_Server_Update_Map();
-        }
-        break;
-        case NMS_LOBBY_SERVER_JOIN: { msg = new LobbyMessage_Server_Join();
-        }
-        break;
+        case NMS_LOBBY_SERVER_ADD: msg = new LobbyMessage_Server_Add(); break;
+        case NMS_LOBBY_SERVER_ADD_FAILED: msg = new LobbyMessage_Server_Add_Failed(); break;
+        case NMS_LOBBY_SERVER_DELETE: msg = new LobbyMessage_Server_Delete(); break;
+        case NMS_LOBBY_SERVER_UPDATE_PLAYER: msg = new LobbyMessage_Server_Update_Player(); break;
+        case NMS_LOBBY_SERVER_UPDATE_MAP: msg = new LobbyMessage_Server_Update_Map(); break;
+        case NMS_LOBBY_SERVER_JOIN: msg = new LobbyMessage_Server_Join(); break;
 
-        case NMS_LOBBY_RANKING_INFO: { msg = new LobbyMessage_Lobby_Ranking_Info();
-        }
-        break;
+        case NMS_LOBBY_RANKING_INFO: msg = new LobbyMessage_Lobby_Ranking_Info(); break;
     }
 
     return msg;
